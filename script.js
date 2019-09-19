f815d733e20acbde0f0e64fc90365e5d61567317
TomoyasuTaka <>
2019-09-20 02:36:46 +0900

    add JavaScript追加 画像が切り替わるように

---
+++ script.js
@@ -0,0 +1,30 @@
+// 全部の要素がマウスホバーで変わるとか楽しくない？
+// マウスホバーで透明になっちゃうとか
+$(function(){
+  $('.profile-image').fadeIn('slow');
+  $('.message').hover(()=>{
+    $('.message').fadeOut();
+  });
+  
+  //ヘッダーロゴの切り替え
+  $('#header-logo').hover(
+    function(){
+      $(this).attr('src', $(this).attr('src').replace('logo', 'logo-jap'));
+  },function(){
+    $(this).attr('src', $(this).attr('src').replace('logo-jap', 'logo'));
+  });
+    //profile-imageの切り替え
+  $('.profile-image').hover(
+    function(){
+      $(this).attr('src', $(this).attr('src').replace('icon', 'icon-shock'));
+  },function(){
+    $(this).attr('src', $(this).attr('src').replace('icon-shock', 'icon'));
+  });
+  $('#top-copy').hover(
+    function(){
+    $(this).text('ぽんぽこぽん！(´>ω<`)');
+  },function(){
+    $(this).text('パンパカパン！( ^ω^ )');
+  });
+  
+});
