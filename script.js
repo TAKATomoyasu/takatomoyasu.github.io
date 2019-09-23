1d09df97e2b8610c4990442cb0f1cd967c1af159
TomoyasuTaka <>
2019-09-24 05:30:05 +0900

    mainページ 	html｜画像を絶対パスに変更 	css｜mouse追加、PenCodeでコンパイル 	js｜クリックでマウスが増加を追加

--- script.js
+++ script.js
@@ -1,31 +1,117 @@
 // 全部の要素がマウスホバーで変わるとか楽しくない？
 // マウスホバーで透明になっちゃうとか
-$(function(){
-  $('.profile-image').fadeIn('slow');
-  $('.message').click(()=>{
+$(function() {
+  $(".profile-image").fadeIn("slow");
+  $(".message").click(() => {
     // $('.message').fadeOut();
-    $('.message').text('まだ作り途中だもん');
+    $(".message").text("まだ作り途中だもん");
   });
-  
+
   //ヘッダーロゴの切り替え
-  $('#header-logo').hover(
-    function(){
-      $(this).attr('src', $(this).attr('src').replace('logo', 'logo-jap'));
-  },function(){
-    $(this).attr('src', $(this).attr('src').replace('logo-jap', 'logo'));
+  $("#header-logo").hover(
+    function() {
+      $(this).attr(
+        "src",
+        $(this)
+          .attr("src")
+          .replace("logo", "logo-jap")
+      );
+    },
+    function() {
+      $(this).attr(
+        "src",
+        $(this)
+          .attr("src")
+          .replace("logo-jap", "logo")
+      );
+    }
+  );
+  //profile-imageの切り替え
+  $(".profile-image").hover(
+    function() {
+      $(this).attr(
+        "src",
+        $(this)
+          .attr("src")
+          .replace("icon", "icon-shock")
+      );
+    },
+    function() {
+      $(this).attr(
+        "src",
+        $(this)
+          .attr("src")
+          .replace("icon-shock", "icon")
+      );
+    }
+  );
+  $("#top-copy").hover(
+    function() {
+      $(this).text("ぽんぽこぽん！(´>ω<`)");
+    },
+    function() {
+      $(this).text("パンパカパン！( ^ω^ )");
+    }
+  );
+
+  //マウス移動時のイベントをBODYタグに登録する
+  document.body.addEventListener("mousemove", function(e) {
+    //座標を取得する
+    var mX = e.pageX; //X座標
+    var mY = e.pageY; //Y座標
+
+    for (var i = 0; i < mouseList.length; i++) {
+      $("." + mouseList[i].name).css({
+        left:
+          mX +
+          mouseList[i].ofsX +
+          mouseList[i].ofsXMul *
+            Math.cos((mouseList[i].ofsAngle + mY) * (Math.PI / 180)) *
+            30,
+        top:
+          mY +
+          mouseList[i].ofsY +
+          mouseList[i].ofsYMul *
+            Math.sin((mouseList[i].ofsAngle + mX) * (Math.PI / 180)) *
+            30
+      });
+    }
+
+    //座標を表示する
+    document.getElementById("txtX").value = mX;
+    document.getElementById("txtY").value = mY;
   });
-    //profile-imageの切り替え
-  $('.profile-image').hover(
-    function(){
-      $(this).attr('src', $(this).attr('src').replace('icon', 'icon-shock'));
-  },function(){
-    $(this).attr('src', $(this).attr('src').replace('icon-shock', 'icon'));
-  });
-  $('#top-copy').hover(
-    function(){
-    $(this).text('ぽんぽこぽん！(´>ω<`)');
-  },function(){
-    $(this).text('パンパカパン！( ^ω^ )');
-  });
-  
+
+  // サブマウスの個数
+  var counter = 0;
+  var mouseList = [];
+
+  // クリック時に、imgタグを追加し、マウスの情報を記録するオブジェクトを生成＆配列に入れる
+  document.body.onclick = function() {
+    counter++;
+    //   タグをhtmlに追加
+    $("body").append(
+      '<img srcset="https://taka-chin.com/images/cursor.png 2.5x" class="mouse m' +
+        counter +
+        '"></img>'
+    );
+    // console.log(
+    //   '<img srcset="https://taka-chin.com/images/cursor.png 2.5x" class="mouse m' +
+    //     counter +
+    //     '"></img>'
+    // );
+
+    //マウスの名前や値を格納するオブジェクトを配列で管理する
+    mouseList.push({
+      name: "m" + counter,
+      ofsX: Math.random() * 60 - 30,
+      ofsY: Math.random() * 60 - 30,
+      amp: 30,
+      //ofsXのsinを足すか引くかを決定。1 - 2 * 0|2という形で、-1 / +1 を作りそれを掛け算する
+      ofsXMul: 1 - 2 * Math.floor(Math.random() * 2),
+      ofsYMul: 1 - 2 * Math.floor(Math.random() * 2),
+      ofsAngle: Math.random() * 360
+    });
+    // console.log(mouseList);
+  };
 });
