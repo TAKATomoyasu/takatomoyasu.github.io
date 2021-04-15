cadf9a535df163adad93c8b12de5ec05167c7aad
TomoyasuTaka <piano.cembalo.taka.2nd@gmail.com>
2021-04-15 22:09:22 +0900

    背景にp5jsを設定完了

--- mochimochi_pointer/sketch.js
+++ mochimochi_pointer/sketch.js
@@ -9,14 +9,18 @@ let radius = 100;
 
 const points = [];
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  canvasSetup;
+}
 function setup() {
-  createCanvas(800, 800);
+  canvas = createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(255);
   noStroke();
-  fill(255,200,200);
+  fill(255, 200, 200);
 
   x += (mouseX - x) / 5.0;
   y += (mouseY - y) / 5.0;
@@ -43,23 +47,23 @@ function draw() {
 
 
 function mousePressed() {
-  
+
   radius *= 0.9;
 }
-function mouseReleased(){
+function mouseReleased() {
   points.push(new PointerClick(mouseX, mouseY, 100));
   radius = 100;
 }
 
 
 class PointerClick {
-  
-  constructor(x, y,effect_size) {
+
+  constructor(x, y, effect_size) {
     this.x = x;
     this.y = y;
     this.effect_size = effect_size;
   }
-  
+
   update() {
     fill(255, 200, 200, 255 - this.effect_size);
     ellipse(this.x, this.y, this.effect_size, this.effect_size);
